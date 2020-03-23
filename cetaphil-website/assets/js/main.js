291903428478dd38e067e6641d7baebeaaeaebfb
sharebear1000 <61988255+sharebear1000@users.noreply.github.com>
2020-03-22 21:37:54 -0500

    Add files via upload

---
+++ cetaphil-website/assets/js/main.js
@@ -0,0 +1,37 @@
+$(function() {
+
+	// Vars.
+		var	$window = $(window),
+			$body = $('body'),
+			$wrapper = $('#wrapper');
+
+	// Breakpoints.
+		skel.breakpoints({
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 736px)',
+			xsmall:	'(max-width: 480px)'
+		});
+
+
+	// Poptrox.
+		$window.on('load', function() {
+
+			$('.thumbnails').poptrox({
+				onPopupClose: function() { $body.removeClass('is-covered'); },
+				onPopupOpen: function() { $body.addClass('is-covered'); },
+				baseZIndex: 10001,
+				useBodyOverflow: false,
+				usePopupEasyClose: true,
+				overlayColor: '#ffffff',
+				overlayOpacity: 0.75,
+				popupLoaderText: '',
+				fadeSpeed: 500,
+				usePopupDefaultStyling: false,
+				windowMargin: (skel.breakpoint('small').active ? 5 : 50)
+			});
+
+		});
+
+});
